09a0808f8044e118d4b546eb68212e7b02b41b8c
Driftini <yurisilveos741@gmail.com>
2023-10-23 11:31:47 +0200

    Initial V3 support, tags are still missing

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Fumoedit
 A simple Python module I plan to use as the backbone for [a proper editor](https://github.com/Driftini/fumoedit-qt) for [my website](https://driftini.github.io)'s posts.
 
-## Progress
-* [x] (Picture)Post class
-* [x] Generation of posts
-* [ ] Reading a post file and instancing a Post class from it
-* [ ] PostCollection class
+## Post version glossary
+* **V1**: introduced with FumoNet.
+* **V2**: revision of V1 introduced with Smoothie's 2022 update.
+    * Transition to YAML's simpler syntax (different indentation, no brackets).
+    * Picture variants are moved from pictures' `lowres` and `maxres` properties to an array.
+* **V3**: major restructuring introduced with FumoNet's 2023 redesign.
+    * Picture thumbnails no longer have an horizontal offset.
+    * Picture thumbnails' offset uses a percentage instead of pixels.
+    * Picture variants removed.
+    * Pictures' properties are now `thumbnail`, `thumbpos`, `original` and `label`.
+    * Posts have tags.
+    
--- __init__.py
+++ __init__.py
@@ -3,69 +3,25 @@ from os import path
 import re
 import yaml
 
+CURRENT_POST_VERSION = 3
+
 
 class Picture:
     def __init__(self, collection):
-        self.thumbnail_name = ""
-        self.thumbnail_offset = ["center", "center"]
-        self.variants = []
-        self.collection = collection  # Reference to the post's collection
-
-    def new_variant(self):
-        v = PictureVariant(self.collection)
-        self.variants.append(v)
-
-        return v
-
-    def get_collection(self):
-        return self.collection[0]
-
-    def get_thumbnail_path(self):
-        return f"/assets/img/posts/{self.get_collection()}/thumbs/{self.thumbnail_name}"
-
-    def get_full_offset(self):
-        x = self.thumbnail_offset[0]
-        y = self.thumbnail_offset[1]
-
-        if x != "center":
-            x = f"{x}px"
-        if y != "center":
-            y = f"{y}px"
-
-        return f"{x} {y}"
-
-    def get_variants_dicts(self):
-        dicts = []
-
-        for v in self.variants:
-            dicts.append(v.get_dict())
-
-        return dicts
-
-    def get_dict(self):
-        d = {
-            "thumbnail": self.get_thumbnail_path(),
-            "thumbpos": self.get_full_offset(),
-            "variants": self.get_variants_dicts()
-        }
-        return d
-
-    def generate(self):
-        gen = yaml.dump(self.get_dict())
-        return gen
-
-
-class PictureVariant:
-    def __init__(self, collection):
-        self.filename = ""
+        self.original_filename = ""
+        self.thumbnail_filename = ""
+        self.thumbnail_offset = 50
         self.label = ""
         self.collection = collection  # Reference to the post's collection
 
     def get_collection(self):
         return self.collection[0]
 
-    def get_path(self):
-        return f"/assets/img/posts/{self.get_collection()}/{self.filename}"
+    def get_original_path(self):
+        return f"/assets/img/posts/{self.get_collection()}/{self.original_filename}"
+
+    def get_thumbnail_path(self):
+        return f"/assets/img/posts/{self.get_collection()}/thumbs/{self.thumbnail_filename}"
 
     def has_label(self):
         if len(self.label) > 0:
@@ -81,7 +37,9 @@ class PictureVariant:
 
     def get_dict(self):
         d = {
-            "file": self.get_path()
+            "thumbnail": self.get_thumbnail_path(),
+            "thumbpos": self.thumbnail_offset,
+            "original": self.get_original_path(),
         }
 
         if self.has_label():
@@ -89,12 +47,16 @@ class PictureVariant:
 
         return d
 
+    def generate(self):
+        gen = yaml.dump(self.get_dict())
+        return gen
+
 
 class Post:
     def __init__(self):
         self.id = "blankpost"  # Used in the internal name
         self.title = "Blank Post"  # Display name
-        self.thumbnail = ""  # Optional, thumbnail path/name???
+        self.priority_thumbnail = ""  # Optional thumbnail override
         self.date = date.today()
         self.body = ""
         self.pictures = []  # Attached pictures
@@ -134,13 +96,12 @@ class Post:
         return f"{self.get_internal_name()}.md"
 
     def get_thumbnail_path(self):
-        return f"/assets/img/posts/{self.get_collection()}/{self.thumbnail}"
+        collection_foldername = {self.get_collection()}
 
-    def get_excerpt(self):
-        # Trimmed body, used in collection index pages
-        body_substring = self.body.slice(0, 500)
+        if collection_foldername == "posts":
+            collection_foldername = "blog"
 
-        return f"{body_substring}..."
+        return f"/assets/img/posts/{collection_foldername}/thumbs/{self.priority_thumbnail}"
 
     def get_pictures_dicts(self):
         dicts = []
@@ -152,14 +113,15 @@ class Post:
 
     def get_dict(self):
         d = {
-            "title": self.title
+            "title": self.title,
+            "version": CURRENT_POST_VERSION
         }
 
         if self.is_picturepost():
             d.update({"pictures": self.get_pictures_dicts()})
 
-        if len(self.thumbnail) > 0:
-            d.update({"thumbnail": self.get_thumbnail_path()})
+        if len(self.priority_thumbnail) > 0:
+            d.update({"prioritythumb": self.get_thumbnail_path()})
 
         return d
 
@@ -188,10 +150,6 @@ def filename_valid(filename):
         return False
 
 
-def get_foldername(filepath):
-    return filepath.split("/")[-2]
-
-
 def post_to_file(post, folderpath):
     # Post files can be freely saved to any folder
     # (collection info may be lost because of poor design)
@@ -204,6 +162,8 @@ def post_to_file(post, folderpath):
 
 
 def post_from_file(filepath):
+    # Determine the version of the given post file,
+    # and call the according post_from_vX method
     basename = path.basename(filepath)
 
     if filename_valid(basename):
@@ -211,25 +171,50 @@ def post_from_file(filepath):
             file_content = f.read()
 
         try:
-            compat_mode = False
             parts = file_content.split("---\n", 2)
+            parts.pop(0)
+            # The split stops at index 2 to account
+            # for separators in the post body (---)
 
-            if "[" in parts[1]:
-                # Square brackets were used in pre-FumoEdit posts,
+            # First part is front matter,
+            # second part is post body
+
+            # First of all, check whether or not the post is up to date
+            post_version = CURRENT_POST_VERSION
+
+            if (
+                "[" in parts[0]
+                or "maxres" in parts[0]
+                or "lowres" in parts[0]
+            ):
+                # Square brackets were used in pre-FumoEdit (V1) posts,
                 # whose indentation breaks PyYAML and that have
-                # a slightly different structure (hardcoded variant names)
-                compat_mode = True
+                # hardcoded variant names (lowres/maxres)
+                post_version = 1
+            elif "variants:" in parts[0]:
+                # Picture variants were last used in
+                # pre-FumoNet redesign (V2) posts
+                post_version = 2
 
-            if compat_mode:
-                # Remove indentation to make the file readable by PyYAML
-                lines = parts[1].split("\n")
-                parts[1] = ""
+            if post_version < CURRENT_POST_VERSION:
+                # If the post needs to be upgraded from an older version,
+                # make a backup before upgrading
+                with open(f"{filepath}.bak",
+                          mode="w", encoding="utf-8") as f:
+                    f.write(file_content)
 
-                for line in lines:
-                    parts[1] += line.lstrip() + "\n"
+                # Upgrade the post one version at a time,
+                # until it's fully up to date
+                while post_version < CURRENT_POST_VERSION:
+                    print(f"{basename} V{post_version} front matter:\n---\n{parts[0]}\n---")
+                    print(f"Upgrading {basename} from V{post_version} to V{post_version + 1}...")
+                    exec(f"parts[0] = upgrade_from_v{post_version}({parts})")
+                    post_version += 1
+                    print("Successful!")
 
-            props = yaml.load(parts[1], yaml.Loader)
-            body = parts[2]
+            # Now that the post is up to date, it can be loaded normally
+            props = yaml.load(parts[0], yaml.Loader)
+            body = parts[1]
             body = body[:-1]  # Erase trailing newline
 
             # Setup post metadata (date, ID, collection)
@@ -241,7 +226,7 @@ def post_from_file(filepath):
             post.id = metadata[3]
 
             # Retrieve collection from the post's containing folder's name
-            collection_name = get_foldername(filepath)
+            collection_name = path.dirname(filepath)
             collection_name = collection_name[1:]  # shave off the underscore
 
             post.set_collection(collection_name)
@@ -249,50 +234,21 @@ def post_from_file(filepath):
             # Setup post properties
             post.title = props["title"]
             post.body = body
-            if "thumbnail" in props:
-                thumbpath_prefix = f"/assets/img/posts/{post.get_collection()}/"
-                post.thumbnail = props["thumbnail"].removeprefix(thumbpath_prefix)
+            if "prioritythumb" in props:
+                post.priority_thumbnail = path.basename(props["prioritythumb"])
 
             # Setup post attachments
             if post.is_picturepost() and "pictures" in props:
                 for p in props["pictures"]:
                     picture_obj = post.new_picture()
 
-                    picture_obj.thumbnail_name = path.basename(p["thumbnail"])
-                    picture_obj.thumbnail_offset = p["thumbpos"].split(" ")
+                    picture_obj.original_filename = path.basename(p["original"])
+                    picture_obj.thumbnail_filename = path.basename(p["thumbnail"])
+                    picture_obj.thumbnail_offset = p["thumbpos"]
 
-                    for i in range(0, 2):
-                        if picture_obj.thumbnail_offset[i] != "center":
-                            # Shave off the "px" suffix, if present
-                            if "px" in picture_obj.thumbnail_offset[i]:
-                                picture_obj.thumbnail_offset[i] = picture_obj.thumbnail_offset[i][:-2]
+                    if "label" in p:
+                        picture_obj.label = p["label"]
 
-                            picture_obj.thumbnail_offset[i] = int(
-                                picture_obj.thumbnail_offset[i]
-                            )
-
-                    if not compat_mode:
-                        if "variants" in p:
-                            for v in p["variants"]:
-                                variant_obj = picture_obj.new_variant()
-
-                                variant_obj.filename = path.basename(v["file"])
-                                if "label" in v:
-                                    variant_obj.label = v["label"]
-                    else:
-                        # Pre-FumoEdit posts' pictures didn't have a variant
-                        # list, instead using the following names
-                        outdated_names = ["lowres", "maxres"]
-
-                        for n in outdated_names:
-                            if n in p:
-                                variant_obj = picture_obj.new_variant()
-
-                                variant_obj.filename = path.basename(
-                                    p[n]["file"]
-                                )
-                                if "label" in p[n]:
-                                    variant_obj.label = p[n]["label"]
             return post
         except yaml.scanner.ScannerError:
             raise
@@ -300,6 +256,57 @@ def post_from_file(filepath):
             raise
     else:
         raise PostNameError
+
+
+def upgrade_from_v1(parts):
+    # Remove indentation to make the file readable by PyYAML
+    lines = parts[0].split("\n")
+    parts[0] = ""
+
+    for line in lines:
+        parts[0] += line.lstrip() + "\n"
+
+    # Replace hardcoded variant names with the variants list
+    hardcoded_names = ["lowres", "maxres"]
+
+    props = yaml.load(parts[0], yaml.Loader)
+
+    if "pictures" in props:
+        for p in props["pictures"]:
+            p["variants"] = []
+
+            for n in hardcoded_names:
+                if n in p:
+                    p["variants"].append(p[n])
+                    p.pop(n)
+
+    parts[0] = yaml.dump(props)
+
+    return parts[0]
+
+
+def upgrade_from_v2(parts):
+    # Set every picture's V3 properties based on the first
+    # variant's properties, while discarding any additional variants
+
+    props = yaml.load(parts[0], yaml.Loader)
+
+    if "pictures" in props:
+        for p in props["pictures"]:
+            p["thumbpos"] = 50
+            # Thumbnail offset gets reset to 50%,
+            # since double px offsets have been dropped
+
+            p["original"] = p["variants"][0]["file"]
+
+            if ("label" in p["variants"][0]):
+                p["label"] = p["variants"][0]["label"]
+
+            p.pop("variants")
+
+    parts[0] = yaml.dump(props)
+
+    return parts[0]
 
 
 # Exceptions
